038025965d328351e0bbb5e24c48735f07a92e70
Manas Ranjan Singh <manassingh900@gmail.com>
2024-08-20 15:14:32 +0530

    add check if the courses is an array or not

--- src/app/admin/instructors/[instructor_id]/courses/page.tsx
+++ src/app/admin/instructors/[instructor_id]/courses/page.tsx
@@ -59,63 +59,64 @@ const MyPublishedCourses = ({ params: { instructor_id } }: Props) => {
     <Box as="main" sx={main} rowGap={5} overflow={"hidden"}>
       <Box overflowY={"scroll"} w={"100%"} h={"100%"} sx={sxScrollbar}>
         <Box display={"grid"} rowGap={5} p={".5rem"}>
-          {courses.map((course, idx) => (
-            <Card
-              key={idx}
-              boxShadow={
-                "rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em"
-              }
-              direction={{ base: "row" }}
-              height={{ base: "100px", sm: "initial" }}
-            >
-              <CardHeader
-                p={{ base: 3, md: 5 }}
-                pr={{ base: 0, sm: "initial" }}
-                flex={1}
+          {Array.isArray(courses) &&
+            courses.map((course, idx) => (
+              <Card
+                key={idx}
+                boxShadow={
+                  "rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em"
+                }
+                direction={{ base: "row" }}
+                height={{ base: "100px", sm: "initial" }}
               >
-                <Image
-                  src={course.courseImg}
-                  alt={course.courseName}
-                  w={"100%"}
-                  h={"100%"}
-                  minW={"100px"}
-                  alignSelf={"center"}
-                  borderRadius={4}
-                />
-              </CardHeader>
-              <CardBody
-                flex={{ base: 4, xl: 5 }}
-                display={"flex"}
-                flexDirection={"column"}
-                rowGap={1}
-                p={{ base: 3, md: 5 }}
-                pr={{ md: 0 }}
-              >
-                <Heading sx={textStyle}>{course.courseName}</Heading>
-                <Grid>
-                  <Text
-                    fontSize={{ base: ".7rem", md: "xs", lg: "sm" }}
-                    color={"#3c7356"}
-                  >
-                    {course.author}
-                  </Text>
-                  <Text
-                    fontSize={{ base: ".7rem", md: "xs", lg: "sm" }}
-                    color={"#3c7356"}
-                  >
-                    {course.courseRating}
-                  </Text>
-                  <Text
-                    fontSize={{ base: ".7rem", md: "xs", lg: "sm" }}
-                    color={"#3c7356"}
-                    display={{ base: "none", sm: "flex" }}
-                  >
-                    {`₹${course.coursePrice.toLocaleString("en-IN")}`}
-                  </Text>
-                </Grid>
-              </CardBody>
-            </Card>
-          ))}
+                <CardHeader
+                  p={{ base: 3, md: 5 }}
+                  pr={{ base: 0, sm: "initial" }}
+                  flex={1}
+                >
+                  <Image
+                    src={course.courseImg}
+                    alt={course.courseName}
+                    w={"100%"}
+                    h={"100%"}
+                    minW={"100px"}
+                    alignSelf={"center"}
+                    borderRadius={4}
+                  />
+                </CardHeader>
+                <CardBody
+                  flex={{ base: 4, xl: 5 }}
+                  display={"flex"}
+                  flexDirection={"column"}
+                  rowGap={1}
+                  p={{ base: 3, md: 5 }}
+                  pr={{ md: 0 }}
+                >
+                  <Heading sx={textStyle}>{course.courseName}</Heading>
+                  <Grid>
+                    <Text
+                      fontSize={{ base: ".7rem", md: "xs", lg: "sm" }}
+                      color={"#3c7356"}
+                    >
+                      {course.author}
+                    </Text>
+                    <Text
+                      fontSize={{ base: ".7rem", md: "xs", lg: "sm" }}
+                      color={"#3c7356"}
+                    >
+                      {course.courseRating}
+                    </Text>
+                    <Text
+                      fontSize={{ base: ".7rem", md: "xs", lg: "sm" }}
+                      color={"#3c7356"}
+                      display={{ base: "none", sm: "flex" }}
+                    >
+                      {`₹${course.coursePrice.toLocaleString("en-IN")}`}
+                    </Text>
+                  </Grid>
+                </CardBody>
+              </Card>
+            ))}
         </Box>
       </Box>
     </Box>
